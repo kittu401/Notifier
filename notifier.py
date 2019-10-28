996273acba5a356976ee3ab1e3cdc87c12eefe82
venkat kishore <19490714+kittu401@users.noreply.github.com>
2019-10-28 11:08:22 +0530

    updated notiifer code
    Added option to close the app or not
    you can add more notification times after finishing given tasks.

--- notifier.py
+++ notifier.py
@@ -11,28 +11,37 @@ def notification(notify_time,notify_message):
     test_time = notify_time
     test_message = notify_message
     for i,j in zip(test_time,test_message):
-        print(i +"....."+j)
 
-        current_time = time.strftime("%H:%M:%S")
-        if current_time == i:
-            print(current_time)
-            hr = ToastNotifier()
-            hr.show_toast("test", j)
-            engine = pyttsx3.init()
-            engine.setProperty('rate', 125)
-            engine.setProperty('voice', en_voice_id)
-            engine.say("You have a notification saying" + j)
-            engine.runAndWait()
-            test_time.remove(i)
-            test_message.remove(j)
-            if len(test_time)==0:
-                engine.say("tasks completed closing application..")
+            current_time = time.strftime("%H:%M:%S")
+            if current_time == i:
+                print(current_time)
+                hr = ToastNotifier()
+                hr.show_toast("test", j)
+                engine = pyttsx3.init()
+                engine.setProperty('rate', 125)
+                engine.setProperty('voice', en_voice_id)
+                engine.say("You have a notification saying" + j)
                 engine.runAndWait()
-                sys.exit()
+                test_time.remove(i)
+                test_message.remove(j)
+                if len(test_time)==0:
+                    engine.say("tasks completed closing application..")
+                    engine.runAndWait()
+                    choice = str(input("want to close app.?"))
+                    if choice=="yes":
+
+                        sys.exit()
+                    elif choice=="no":
+                        times = str(input("Enter time to Notify.: "))
+                        message = input("Enter Message : ")
+                        notify_time2 = times.split(",")
+                        notify_message2 = message.split(",")
+                        while True:
+                            notification(notify_time2,notify_message2)
+                else:
+                    pass
             else:
                 pass
-        else:
-            pass
 while True:
     notification(notify_time,notify_message)
 
